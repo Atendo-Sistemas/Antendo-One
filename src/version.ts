09e6ccb3cfd31278eefd9f33a17a257241b9ad94
Atendo Sistemas <contato@atendo.log.br>
2026-09-19 11:01:28 -0300

    V1.8.35

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = 'v1.8.32';
-export const APP_BUILD_DATE = '14/09/2026';
+export const APP_VERSION = 'V1.8.35';
+export const APP_BUILD_DATE = '19/09/2026';
 export const APP_RELEASE_NAME = 'Menu agrupado para usuários empresariais';
